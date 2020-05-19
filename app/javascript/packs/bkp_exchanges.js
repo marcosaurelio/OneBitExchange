f9b0b8d948d7dc1233d5bbd59b7798a3d23c173e
Marcos Aurelio <marcosaurelio.rn@gmail.com>
2020-05-18 22:54:50 -0300

    Trash

--- app/javascript/packs/bkp_exchanges.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('turbolinks:load', function(){
-  document.getElementById('exchange_form').addEventListener('ajax:success', function(event) {
-    let [result] = event.detail
-    document.getElementById('result').value = result.value
-  })
-}
-
-alert('Teste')
